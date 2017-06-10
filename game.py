46411b6c1862d7bd767b1102bec56fb5f2eb8bb6
Lori <mclorimer@gmail.com>
2017-06-10 18:48:11 -0400

    Some updated stuff

--- game.py
+++ game.py
@@ -9,22 +9,40 @@ def game(goalScore):
 	p1totalScore = []
 	p2turnScore = []
 	p2totalScore = []
-	turn(input("It's player 1's turn. Please type 'r' to roll or 'b' to bank: "))
+	p1totalScore.append(turn(input("It's player 1's turn. Please type 'r' to roll or 'b' to bank: ")))
+	print(p1totalScore)
 
 def turn(choice):
-	dice = []
-	if choice  == "r":
-		randNum1 = randint(1,6)
-		randNum2 = randint(1,6)
-		dice.append(randNum2)
-		dice.append(randNum1)
-		if (randNum1+randNum2) == 2:
-			print("snake eyes!!")
-		else:
-			print("You rolled: ", dice)
-	dicesum = sum(dice)
-	print("The turn score is: ", dicesum, "enter 'r' to reroll or 'b' to bank!")
-	return dicesum
+	turnOn = True
+	while turnOn == True:
+		turnscore = [0]
+		dice = []
+		dicesum = 0
+		if choice  == "r":
+			notEnd = True
+			while notEnd == True:
+				randNum1 = randint(1,6)
+				randNum2 = randint(1,6)
+				dice.append(randNum2)
+				dice.append(randNum1)
+				if (randNum1+randNum2) == 2:
+					notEnd = False
+					print("Snake eyes!! ", dice)
+					turnOn = False
+
+				else:
+					print("You rolled: ", dice)
+					dicesum = sum(dice)
+					turnscore.append(dicesum)
+					notEnd = False
+		print("The turn score is: ", dicesum)
+		if choice == "b":
+			turnOn = False
+	return sum(turnscore)
+
+
+
+
 
 
 game(goalScore)
