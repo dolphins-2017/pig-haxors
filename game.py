284e43219f2af7497857b7e65a7fa8b6ba7002c1
Lori <mclorimer@gmail.com>
2017-06-10 18:00:47 -0400

    just a couple of functions.

--- game.py
+++ game.py
@@ -1,2 +1,30 @@
-class Game:
-	pass
+from random import randint
+
+
+goalScore = input("Please enter the score you would like to count up to: ")
+
+
+def game(goalScore):
+	p1turnScore = []
+	p1totalScore = []
+	p2turnScore = []
+	p2totalScore = []
+	turn(input("It's player 1's turn. Please type 'r' to roll or 'b' to bank: "))
+
+def turn(choice):
+	dice = []
+	if choice  == "r":
+		randNum1 = randint(1,6)
+		randNum2 = randint(1,6)
+		dice.append(randNum2)
+		dice.append(randNum1)
+		if (randNum1+randNum2) == 2:
+			print("snake eyes!!")
+		else:
+			print("You rolled: ", dice)
+	dicesum = sum(dice)
+	print("The turn score is: ", dicesum, "enter 'r' to reroll or 'b' to bank!")
+	return dicesum
+
+
+game(goalScore)
