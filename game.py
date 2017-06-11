a32a060c0c504e7ee3c5faffb1eade3761d1c59d
Mira Chugh <mirachugh@Miras-MacBook-Pro.local>
2017-06-11 15:39:19 -0400

    more changes, but doesn't run yet..

--- game.py
+++ game.py
@@ -4,64 +4,66 @@ from random import randint
 goalScore = input("Please enter the score you would like to count up to: ")
 player = player1
 
-def game(goalScore, player):
+def game(goalScore, player, turnscore):
 	if player == player1:
-		turnscore = turn(input("It's player 1's turn. Please type 'r' to roll or 'b' to bank: "))
+		turnscore = 0
+		turnscore = turn(player, input("It's player 1's turn. Please type 'r' to roll or 'b' to bank: ", turnscore))
 		p1totalScore += turnscore
 		if p1totalScore >= goalScore:
 			return ("Player 1 has won the game with a score of " + str(p1totalScore))
 		else:
-			return (game(goalScore, player2))
+			player = player2
+			return (game(goalScore, player))
 	else:
 		turnscore = turn(input("It's player 2's turn. Please type 'r' to roll or 'b' to bank: "))
 		p2totalScore += turnscore
 		if p2totalScore >= goalScore:
 			return ("Player 2 has won the game with a score of " + str(p1totalScore))
 		else:
-			return (game(goalScore, player1))
+			player = player1
+			return (game(goalScore, player))
 
 #after each roll prompt user to roll  or bank again
-def turn(choice):
-	turnOn = True
-	while turnOn == True:
-		turnscore = 0
-		dice = []
-		dicesum = 0
-		if choice  == "r":
-			notEnd = True
-			while notEnd == True:
-				randNum1 = randint(1,6)
-				randNum2 = randint(1,6)
-				dice.append(randNum2)
-				dice.append(randNum1)
-				if (randNum1+randNum2) == 2:
-					notEnd = False
-					print("Snake eyes!! ", dice)
-					turnOn = False
-					#somehow set total score = to 0
-				elif randNum1 == 1 or randNum2 == 1:
-					turnscore = 0
-					notEnd = False
-					turnOn = False
-				elif randNum1 == randNum2:
-					turnscore += dicesum
-					#can't bank: print(You rolled 2 of the same number, press r to toll again!)
-
-				else:
-					print("You rolled: ", dice)
-					dicesum = sum(dice)
-					turnscore += dicesum
-					notEnd = False
-		print("The turn score is: ", dicesum)
+def turn(player, choice, turnscore):
+	dice = []
+	dicesum = 0
+	if choice  == "r":
+		randNum1 = randint(1,6)
+		randNum2 = randint(1,6)
+		dice.append(randNum2)
+		dice.append(randNum1)
+		if (randNum1+randNum2) == 2:
+			print("Snake eyes!! ", dice)
+			print ("Your total score will be reset to 0 and your turn is over.")
+			turnscore = "RESET"
+			return (game(goalScore, player, turnscore))
+		elif randNum1 == 1 or randNum2 == 1:
+			print ("You rolled: ", dice)
+			print ("Your score for this turn is 0 and your turn is over.")
+			turnscore = 0
+			return (game(goalScore, player, turnscore))
+			
+		elif randNum1 == randNum2:
+			print ("You rolled: ", dice)
+			dicesum = sum(dice)
+			print ("Since you rolled 2 of the same number, " + str(dicesum) + " will be added to your score for this turn and you have to roll again.")
+			turnscore += dicesum
+			return (turn (player, (input("Please type r to roll again: ")), turnscore))
 
 		else:
+			print("You rolled: ", dice)
+			dicesum = sum(dice)
 			turnscore += dicesum
-			turnOn = False
-	return turnscore
+			print ("The score for your turn is now " + str(turnscore))
+			return (turn (player, (input("Type r to roll again or b to bank: ")), turnscore))
+	else:
+		turnscore += dicesum
+		return (game(goalScore, player, turnscore))
 
 
 
 
 
 
-game(goalScore)
+
+game(goalScore, player)
